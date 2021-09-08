9d3cf1976e84dd9d26d9edbddacde9ff7c658db5
aorich <rich.aidan@gmail.com>
2021-09-08 09:51:19 -0400

    testing js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+prompt ("testing");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
