298807a95843015ea6e11e43c4e36e0c3f30b021
aorich <rich.aidan@gmail.com>
2021-09-08 20:22:18 -0400

    arrays added from internet

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # password-generator
 
-credits: https://www.youtube.com/watch?v=duNmhKgtcsI
+credits: https://www.youtube.com/watch?v=duNmhKgtcsI arrays
+
+        https://codepen.io/FlorinPop17/pen/BaBePej arrays and code
--- assets/js/script.js
+++ assets/js/script.js
@@ -21,7 +21,27 @@ function generatePassword () {
   var useNumber = confirm("Use numbers?");
   var useSymbol = confirm("Use symbols?");
 
+    
 
+
+
+  function getRandomLower() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+  }
+  
+  function getRandomUpper() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+  }
+  
+  function getRandomNumber() {
+    return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+  }
+  
+  function getRandomSymbol() {
+    const symbols = '!@#$%^&*(){}[]=<>/,.'
+    return symbols[Math.floor(Math.random() * symbols.length)];
+  }
+  
 
   // return pizza;
 }
