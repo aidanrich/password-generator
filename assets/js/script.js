68388095747e2388be28492e7cfc3eb3685411dd
aorich <rich.aidan@gmail.com>
2021-09-09 15:43:04 -0400

    big test! added a bunch of possibly broken code

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,42 +11,58 @@ function writePassword() {
 
 }
 
-function generatePassword () {
+function generatePassword() {
   // var superSecretPass;
   // var pizza = confirm("Do you like pizza?");
   // // This is where the challenge for this should happen. confirm gets you prompts
-  var passLength = prompt("How many characters? Must be between 8 and 128");
+  var myPassword = ()
+  var lengthOfPass = prompt("How many characters? Must be between 8 and 128");
+  if (8 > lengthOfPass || lengthOfPass > 128) {
+    alert("Password must be between 8 and 128 characters.");
+    return;
+  }
+
   var lowerCase = confirm("Use lower case letters?");
+  if (lowerCase) {
+    getRandomLower()
+  }
   var upperCase = confirm("Use upper case letters?");
+  if (upperCase) {
+    getRandomUpper()
+  }
   var useNumber = confirm("Use numbers?");
+  if (useNumber) {
+    getRandomNumber
+  }
   var useSymbol = confirm("Use symbols?");
-
-    if (passLength < 8 && passLength > 128) {
-      
-    }
-
-
-
-  function getRandomLower() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+  if (useSymbol) {
+    getRandomSymbol
   }
-  
-  function getRandomUpper() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-  }
-  
-  function getRandomNumber() {
-    return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-  }
-  
-  function getRandomSymbol() {
-    const symbols = '!@#$%^&*(){}[]=<>/,.'
-    return symbols[Math.floor(Math.random() * symbols.length)];
-  }
-  
+}
+
+
+
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber() {
+  return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+  const symbols = '!@#$%^&*(){}[]=<>/,.'
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+return myPassword()
 
   // return pizza;
-}
+
 
 
 // Add event listener to generate button
