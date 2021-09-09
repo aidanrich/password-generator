df5620a2a8ea45ea80af65b0fd5bbd2c6a00f0f4
aorich <rich.aidan@gmail.com>
2021-09-09 16:43:07 -0400

    some busted code. working on length of password

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,53 +15,58 @@ function generatePassword() {
   // var superSecretPass;
   // var pizza = confirm("Do you like pizza?");
   // // This is where the challenge for this should happen. confirm gets you prompts
-  var myPassword = ()
+  var myPassword = (lengthOfPass + lowerCase + upperCase + useNumber + useSymbol)
   var lengthOfPass = prompt("How many characters? Must be between 8 and 128");
   if (8 > lengthOfPass || lengthOfPass > 128) {
     alert("Password must be between 8 and 128 characters.");
     return;
+  } else {
+    lengthOfPass.length
   }
 
   var lowerCase = confirm("Use lower case letters?");
   if (lowerCase) {
-    getRandomLower()
+    getRandomLower();
   }
   var upperCase = confirm("Use upper case letters?");
   if (upperCase) {
-    getRandomUpper()
+    getRandomUpper();
   }
   var useNumber = confirm("Use numbers?");
   if (useNumber) {
-    getRandomNumber
+    getRandomNumber();
   }
   var useSymbol = confirm("Use symbols?");
   if (useSymbol) {
-    getRandomSymbol
+    getRandomSymbol();
+  }
+
+
+  return myPassword;
+
+
+
+
+  function getRandomLower() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+  }
+  console.log(getRandomLower())
+
+  function getRandomUpper() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+  }
+
+  function getRandomNumber() {
+    return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+  }
+
+  function getRandomSymbol() {
+    const symbols = '!@#$%^&*(){}[]=<>/,.'
+    return symbols[Math.floor(Math.random() * symbols.length)];
   }
 }
 
-
-
-function getRandomLower() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
-
-function getRandomUpper() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
-
-function getRandomNumber() {
-  return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
-
-function getRandomSymbol() {
-  const symbols = '!@#$%^&*(){}[]=<>/,.'
-  return symbols[Math.floor(Math.random() * symbols.length)];
-}
-
-return myPassword()
-
-  // return pizza;
+// return pizza;
 
 
 
