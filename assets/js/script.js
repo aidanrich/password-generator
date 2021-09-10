b073b23ad0e58af7780ed794326de8d9a656dce2
aorich <rich.aidan@gmail.com>
2021-09-09 20:19:43 -0400

    arrays

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,10 @@ function writePassword() {
 
 }
 
-
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var randomNumber = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var randomSymbols = ["!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "{", "}", "~"] 
 
 function generatePassword() {
   // var superSecretPass;
@@ -27,22 +30,22 @@ function generatePassword() {
 
 
 
-  var lowerCase = confirm("Use lower case letters?");
-  if (lowerCase) {
-    getRandomLower();
-  }
-  var upperCase = confirm("Use upper case letters?");
-  if (upperCase) {
-    getRandomUpper();
-  }
-  var useNumber = confirm("Use numbers?");
-  if (useNumber) {
-    getRandomNumber();
-  }
-  var useSymbol = confirm("Use symbols?");
-  if (useSymbol) {
-    getRandomSymbol();
-  }
+  // var lowerCase = confirm("Use lower case letters?");
+  // if (lowerCase) {
+  //   getRandomLower();
+  // }
+  // var upperCase = confirm("Use upper case letters?");
+  // if (upperCase) {
+  //   getRandomUpper();
+  // }
+  // var useNumber = confirm("Use numbers?");
+  // if (useNumber) {
+  //   getRandomNumber();
+  // }
+  // var useSymbol = confirm("Use symbols?");
+  // if (useSymbol) {
+  //   getRandomSymbol();
+  // }
   
   for ( var i = 0; i < lengthOfPass; i++ ) {
     result += characters.charAt(Math.floor(Math.random() * 
@@ -54,26 +57,10 @@ lengthOfPass));
 }
 
 
-  function getRandomLower() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-  }
-  console.log(getRandomLower())
-
-  function getRandomUpper() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-  }
-
-  function getRandomNumber() {
-    return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-  }
-
-  function getRandomSymbol() {
-    const symbols = '!@#$%^&*(){}[]=<>/,.'
-    return symbols[Math.floor(Math.random() * symbols.length)];
-  }
+  
 
 
-// return pizza;
+
 
 
 
