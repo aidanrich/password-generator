59ba0fb8d0cefbd04f215186a4616fbe759819f8
aorich <rich.aidan@gmail.com>
2021-09-09 21:14:24 -0400

    more array work

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,14 +11,15 @@ function writePassword() {
 
 }
 
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var randomLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var randomUpper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var randomNumber = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var randomSymbols = ["!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "{", "}", "~"] 
 
+
+
 function generatePassword() {
-  // var superSecretPass;
-  // var pizza = confirm("Do you like pizza?");
+
   // // This is where the challenge for this should happen. confirm gets you prompts
   var myPassword = "";
   var lengthOfPass = prompt("How many characters? Must be between 8 and 128");
@@ -28,28 +29,23 @@ function generatePassword() {
   }
   console.log(lengthOfPass)
 
+  var passComponents = []
+
+  var lowerCaseConfirm = confirm("Use lower case letters?");
+    if(lowerCaseConfirm) {
+        myPassword = passComponents + randomLower
+    }
 
 
-  // var lowerCase = confirm("Use lower case letters?");
-  // if (lowerCase) {
-  //   getRandomLower();
-  // }
-  // var upperCase = confirm("Use upper case letters?");
-  // if (upperCase) {
-  //   getRandomUpper();
-  // }
-  // var useNumber = confirm("Use numbers?");
-  // if (useNumber) {
-  //   getRandomNumber();
-  // }
-  // var useSymbol = confirm("Use symbols?");
-  // if (useSymbol) {
-  //   getRandomSymbol();
-  // }
+  var upperCaseConfirm = confirm("Use upper case letters?");
+  
+  var useNumberConfirm = confirm("Use numbers?");
+  
+  var useSymbolConfirm = confirm("Use symbols?");
+  
   
   for ( var i = 0; i < lengthOfPass; i++ ) {
-    result += characters.charAt(Math.floor(Math.random() * 
-lengthOfPass));
+    myPassword = passComponents;
  }
 
   return myPassword;
